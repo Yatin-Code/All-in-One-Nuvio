53f49ba6eb1c5f42b48819ea91f8da9545eb8659
Yatin Jain <192596591+Yatin-Code@users.noreply.github.com>
2026-05-24 18:43:18 +0530

    Update UHDMovies provider domain

--- providers/uhdmovies.js
+++ providers/uhdmovies.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-var DOMAIN = "https://uhdmovies.pink";
+var DOMAIN = "https://uhdmovies.rodeo";
 var TMDB_API = "https://api.themoviedb.org/3";
 var TMDB_API_KEY = "1865f43a0549ca50d341dd9ab8b29f49";
 var USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
